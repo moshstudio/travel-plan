d65a3ce059976a404356ff9a86b65040b0be7622
cf <38805177+cuifengcn@users.noreply.github.com>
2025-05-28 17:25:32 +0800

    Updates

---
+++ assets/TravelPlan-k6Lqvmii.js
@@ -0,0 +1,1 @@
+var d=(e=>(e.planned="planned",e.upcoming="upcoming",e.inProgress="in-progress",e.expired="expired",e.completed="completed",e.cancelled="cancelled",e.deleted="deleted",e))(d||{});export{d as T};
